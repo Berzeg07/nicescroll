dbfce014cd426a72c90a9c35e750f5bbe8ed618a
Berzeg07 <berezg86@gmail.com>
2017-05-31 12:50:32 +0400

    29.14

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $(".js-nicescroll").niceScroll({
+        cursorcolor: "red",
+        background: "blue",
+        cursorwidth: "5px",
+        cursorborder: "0",
+        cursorborderradius: "0",
+        autohidemode: false,
+        cursorminheight: 50,
+        horizrailenabled: false,
+        railpadding: { top: -5, right: 0, left: 0, bottom: 5 },
+    });
+
+});
